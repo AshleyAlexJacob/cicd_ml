159dbaba26f3e80aa228fa3d5fda83bd462d3556
AshleyAlexJacob <alexjacob260@gmail.com>
2024-08-16 00:36:58 +0500

    pipeline test

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.10.13 as build
+WORKDIR /app
+
+# copy files
+COPY ./models/ ./models/
+COPY ./model_v1.0.pkl ./model_v1.0.pkl
+COPY ./prediction.py ./prediction.py
+COPY ./requirements.txt ./requirements.txt
+
+ENV VIRTUAL_ENV=/home/packages/.venv
+ADD https://astral.sh/uv/install.sh /install.sh
+RUN chmod -R 777 /install.sh && /install.sh && rm /install.sh
+
+RUN /root/.cargo/bin/uv venv /home/packages/.venv
+
+RUN /root/.cargo/bin/uv pip install --no-cache --system  -r requirements.txt
+
+CMD ["python", "prediction.py"]
---
+++ prediction.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import joblib
+import uvicorn
+from models.input import Input
+import pandas as pd
+
+app = FastAPI()
+
+def load_model(path:str=None):
+    path = path if path is not None else "model_v1.0.pkl"
+    return joblib.load(path)
+
+model = load_model("model_v1.0.pkl")
+
+
+@app.post("/predict")
+def predict(data:Input):
+    try:
+        data_dict = data.model_dump()
+        model_input =  pd.DataFrame([data_dict])
+        prediction  = model.predict(model_input)[0]
+        return JSONResponse({"result": prediction}, 200)
+    except Exception as e:
+        return JSONResponse({"error": "Internal Server Error"}, 404)
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port= 8000)
