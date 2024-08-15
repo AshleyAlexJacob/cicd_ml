bd303cc3d85c1e17fc43d689bc628ba0c1a17847
AshleyAlexJacob <alexjacob260@gmail.com>
2024-08-16 01:24:45 +0500

    formatting

--- models/input.py
+++ models/input.py
@@ -1,10 +1,13 @@
 from typing import Literal
 from pydantic import BaseModel, Field
 
+
 class Input(BaseModel):
     age: int = Field(..., ge=0, description="Age of the client")
     job: str = Field(..., description="Job category of the client")
-    marital: Literal["single", "married", "divorced"] = Field(..., description="Marital status of the client")
+    marital: Literal["single", "married", "divorced"] = Field(
+        ..., description="Marital status of the client"
+    )
     education: str = Field(..., description="Education level of the client")
     default: Literal["yes", "no"] = Field(..., description="Has credit in default?")
     balance: int = Field(..., description="Balance in the client's account")
@@ -12,9 +15,32 @@ class Input(BaseModel):
     loan: Literal["yes", "no"] = Field(..., description="Has personal loan?")
     contact: str = Field(..., description="Contact communication type")
     day: int = Field(..., ge=1, le=31, description="Last contact day of the month")
-    month: Literal["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"] = Field(..., description="Last contact month of the year")
+    month: Literal[
+        "jan",
+        "feb",
+        "mar",
+        "apr",
+        "may",
+        "jun",
+        "jul",
+        "aug",
+        "sep",
+        "oct",
+        "nov",
+        "dec",
+    ] = Field(..., description="Last contact month of the year")
     duration: int = Field(..., ge=0, description="Last contact duration in seconds")
-    campaign: int = Field(..., ge=0, description="Number of contacts performed during this campaign")
-    pdays: int = Field(..., ge=-1, description="Number of days since the client was last contacted from a previous campaign (-1 means client was not previously contacted)")
-    previous: int = Field(..., ge=0, description="Number of contacts performed before this campaign")
-    poutcome: Literal["unknown", "failure", "success", "other"] = Field(..., description="Outcome of the previous marketing campaign")
+    campaign: int = Field(
+        ..., ge=0, description="Number of contacts performed during this campaign"
+    )
+    pdays: int = Field(
+        ...,
+        ge=-1,
+        description="Number of days since the client was last contacted from a previous campaign (-1 means client was not previously contacted)",
+    )
+    previous: int = Field(
+        ..., ge=0, description="Number of contacts performed before this campaign"
+    )
+    poutcome: Literal["unknown", "failure", "success", "other"] = Field(
+        ..., description="Outcome of the previous marketing campaign"
+    )
--- prediction.py
+++ prediction.py
@@ -7,23 +7,25 @@ import pandas as pd
 
 app = FastAPI()
 
-def load_model(path:str=None):
+
+def load_model(path: str = None):
     path = path if path is not None else "model_v1.0.pkl"
     return joblib.load(path)
+
 
 model = load_model("model_v1.0.pkl")
 
 
 @app.post("/predict")
-def predict(data:Input):
+def predict(data: Input):
     try:
         data_dict = data.model_dump()
-        model_input =  pd.DataFrame([data_dict])
-        prediction  = model.predict(model_input)[0]
+        model_input = pd.DataFrame([data_dict])
+        prediction = model.predict(model_input)[0]
         return JSONResponse({"result": prediction}, 200)
     except Exception as e:
         return JSONResponse({"error": f"Internal Server {e}"}, 404)
-    
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port= 8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
